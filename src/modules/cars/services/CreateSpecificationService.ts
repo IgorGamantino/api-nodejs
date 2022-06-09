b8ea96dbf46d0eaab753d4f4b2b27d3e66100446
Igor Amantino <igor.amantino.ext@dasa.com>
2022-06-09 15:25:05 -0300

    create a specification route post

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,20 @@
+import { ISpecificationsRepository } from "../repositories/Specification/ISpecificationsRepository"
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log('execute')
+  constructor(private specificationsRepository: ISpecificationsRepository) { }
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists = this.specificationsRepository.findByNameSpecification(name)
+
+    if (specificationAlreadyExists) {
+      throw new Error('Specification already exists!')
+    }
+
+    this.specificationsRepository.create({ name, description });
   }
 }
 
