959ccfa8348e1f60aa30106d762f0aae8467d436
Igor Amantino <igor.amantino.ext@dasa.com>
2022-07-25 21:02:55 -0300

    refactor remove table username

--- src/modules/account/useCases/createUser/CreateUserController.ts
+++ src/modules/account/useCases/createUser/CreateUserController.ts
@@ -7,11 +7,11 @@ import { CreateUserUseCase } from "./CreateUserUseCase";
 class CreateUserController {
 
   async handle(request: Request, response: Response): Promise<Response> {
-    const { name, email, password, username, driver_license } = request.body;
+    const { name, email, password, driver_license } = request.body;
 
     const createUseCase = container.resolve(CreateUserUseCase);
 
-    await createUseCase.execute({ name, email, password, username, driver_license });
+    await createUseCase.execute({ name, email, password, driver_license });
 
     return response.status(200).send()
   }
